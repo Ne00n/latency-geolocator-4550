eee231e4dd71776243dc8dd013331ecf756043cc
Neoon <neo@tiefkuehler-madness.me>
2021-06-10 14:34:37 +0200

    add corrector

--- Class/geolocator.py
+++ Class/geolocator.py
@@ -1,10 +1,10 @@
-import subprocess, pyasn, time, json, sys, re, os
+import subprocess, random, pyasn, time, json, sys, re, os
 from multiprocessing import Process
 from datetime import datetime
 
 class Geolocator:
 
-    masscanDir,locations,asndb = "","",""
+    masscanDir,locations,pingable,asndb,notPingable = "","","","",""
 
     def __init__(self,masscanDir="/masscan/"):
         print("Loading asn.dat")
@@ -13,6 +13,11 @@ class Geolocator:
         print("Loading locations.json")
         with open(os.getcwd()+"/locations.json", 'r') as f:
             self.locations = json.load(f)
+
+    def loadPingable(self):
+        print("Loading pingable.json")
+        with open(os.getcwd()+"/pingable.json", 'r') as f:
+            self.pingable = json.load(f)
 
     def cmd(self,cmd):
         p = subprocess.run(cmd, stdin=None, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
@@ -63,15 +68,29 @@ class Geolocator:
         with open(os.getcwd()+'/pingable.json', 'w') as f:
             json.dump(list, f)
 
-    def getIPs(self,pingable,row,length=1000):
+    def getIPs(self,row,length=1000):
         list,current,count = [],0,0
-        for subnet,ips in pingable.items():
+        for subnet,ips in self.pingable.items():
             current += 1
             if current >= row:
                 list.append(ips[0])
                 count += 1
             if count == length: return list
         return list
+
+    def SliceAndDice(self,notPingable,row):
+        if row + 1000 > len(notPingable):
+            maximale = len(notPingable)
+        else:
+            maximale = row + 1000
+        return notPingable[row:maximale]
+
+    def SubnetsToRandomIP(self,list):
+        ips = []
+        for subnet in list:
+            if subnet in self.pingable:
+                ips.append(random.choice(self.pingable[subnet]))
+        return ips
 
     def getAvrg(self,result):
         latency = {}
@@ -90,44 +109,55 @@ class Geolocator:
             subnet[lookup[1]] = ms
         return subnet
 
-    def fpingLocation(self,pingable,location):
+    def fpingLocation(self,location,update=False):
         row = 0
-        while row < len(pingable):
-            ips = self.getIPs(pingable,row)
+        length = len(self.pingable)
+        if update: length = len(self.notPingable)
+        while row < length:
+            if update is False: ips = self.getIPs(row)
+            if update is True: ips = self.SliceAndDice(self.notPingable,row)
             current = int(datetime.now().timestamp())
-            print(location['name'],"Running fping")
             cmd = "ssh root@"+location['ip']+" fping -c2 "
             cmd += " ".join(ips)
             result = self.cmd(cmd)
             latency = self.getAvrg(result[1])
             subnets = self.mapToSubnet(latency)
-            print(location['name'],"Updating",location['name']+"-subnets.json")
-            if os.path.exists(os.getcwd()+'/data/'+location['name']+"-subnets.json"):
+            if update is False:
+                print(location['name'],"Updating",location['name']+"-subnets.json")
+                if os.path.exists(os.getcwd()+'/data/'+location['name']+"-subnets.json"):
+                    with open(os.getcwd()+'/data/'+location['name']+"-subnets.json", 'r') as f:
+                        subnetsOld = json.load(f)
+                    subnets = {**subnets, **subnetsOld}
+                with open(os.getcwd()+'/data/'+location['name']+"-subnets.json", 'w') as f:
+                    json.dump(subnets, f)
+            else:
+                print(location['name'],"Merging",location['name']+"-subnets.json")
                 with open(os.getcwd()+'/data/'+location['name']+"-subnets.json", 'r') as f:
-                    subnetsOld = json.load(f)
-                subnets = {**subnets, **subnetsOld}
-            with open(os.getcwd()+'/data/'+location['name']+"-subnets.json", 'w') as f:
-                json.dump(subnets, f)
+                    subnetsCurrent = json.load(f)
+                #print(subnets)
+                for line in subnets.items():
+                    subnetsCurrent[line[0]] = line
+                print(location['name'],"Saving",location['name']+"-subnets.json")
+                with open(os.getcwd()+'/data/'+location['name']+"-subnets.json", 'w') as f:
+                    json.dump(subnetsCurrent, f)
             row += 1000
             currentLoop = int(datetime.now().timestamp())
-            print(location['name'],"Done",row,"of",len(pingable))
+            print(location['name'],"Done",row,"of",length)
             diff = currentLoop - current
-            print(location['name'],"Finished in approximately",round(diff * ( (len(pingable) - row) / 1000) / 60),"minutes")
+            print(location['name'],"Finished in approximately",round(diff * ( (length - row) / 1000) / 60),"minutes")
         print(location['name'],"Done")
 
     def geolocate(self):
         print("Geolocate")
-        print("Loading pingable.json")
-        with open(os.getcwd()+"/pingable.json", 'r') as f:
-            pingable = json.load(f)
-        print("Got",str(len(pingable)),"subnets")
+        self.loadPingable()
+        print("Got",str(len(self.pingable)),"subnets")
 
         for location in self.locations:
             if os.path.exists(os.getcwd()+'/data/'+location['name']+"-subnets.json"):
                 answer = input(location['name']+"-subnets.json already exists. Do you want to rebuild? (y/n): ")
                 if answer != "y": continue
                 os.remove((os.getcwd()+'/data/'+location['name']+"-subnets.json"))
-            p = Process(target=self.fpingLocation, args=([pingable,location]))
+            p = Process(target=self.fpingLocation, args=([location]))
             p.start()
 
     def generate(self):
@@ -164,3 +194,21 @@ class Geolocator:
             export += row[0]+" => "+row[1]['datacenter']+"\n"
         with open(os.getcwd()+'/data/dc.conf', 'w+') as out:
             out.write(export)
+
+    def corrector(self):
+        print("Corrector")
+        notPingable = []
+        for location in self.locations:
+            print("Loading",location['name']+"-subnets.json")
+            with open(os.getcwd()+'/data/'+location['name']+"-subnets.json", 'r') as f:
+                tmp = json.load(f)
+            for line in tmp.items():
+                if line[1] == "retry":
+                    notPingable.append(line[0])
+        notPingable,tmp = list(set(notPingable)),""
+        self.loadPingable()
+        self.notPingable = self.SubnetsToRandomIP(notPingable)
+        notPingable = ""
+        for location in self.locations:
+            p = Process(target=self.fpingLocation, args=([location,True]))
+            p.start()
