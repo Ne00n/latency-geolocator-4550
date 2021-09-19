3817460f20d0874bde125c1d21ae8d5e016af830
Neoon <neo@tiefkuehler-madness.me>
2021-09-19 18:36:33 +0200

    fix

--- tools/cutter.py
+++ tools/cutter.py
@@ -1,8 +1,11 @@
-import geoip2.database, pyasn, json
+import geoip2.database, pyasn, json, sys
 
-with open("pingable.json", 'r') as f:
+#python3 cutter.py country
+print("Loading","pingable.json")
+with open("../pingable.json", 'r') as f:
     pingable =  json.load(f)
 
+print("Loading","GeoLite2")
 reader = geoip2.database.Reader("../GeoLite2-Country.mmdb")
 asndb = pyasn.pyasn('../asn.dat')
 
@@ -17,7 +20,8 @@ for subnet,ips in pingable.items():
     filter.append(lookup[0])
     try:
         response = reader.country(ips[0])
-        if response.country.iso_code == "RU": export.append(ips[0])
+        if response.country.iso_code ==  sys.argv[1]: export.append(ips[0])
+        print("Adding",ips[0])
     except Exception as e:
         print("Skipping",subnet)
 
