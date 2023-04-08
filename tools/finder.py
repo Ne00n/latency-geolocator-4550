1b2f638ca6bab8d7dc05046a77fbe399895bc948
Neoon <neo@tiefkuehler-madness.me>
2023-04-08 11:42:19 +0200

    mtr targets

--- tools/finder.py
+++ tools/finder.py
@@ -1,5 +1,19 @@
 import subprocess, json, sys, re
 
+carriers = {9002:"RETN",3320:"DTAG",3257:"GTT",3356:"Lumen",2914:"NTT",3491:"PCCW",6453:"TATA",1299:"Telia"}
+
+def mtr(target):
+    mtr = ['mtr','--aslookup','--report','--report-cycles','5', target]
+    return subprocess.run(mtr, stdin=None, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=False).stdout.decode('utf-8')
+
+def classify(mtr):
+    carriersOnRoute = ""
+    for line in mtr.split('\n'):
+        asn = re.findall("AS([0-9]+)",line, re.MULTILINE)
+        if asn and int(asn[0]) in carriers:
+            carriersOnRoute += f"{carriers[int(asn[0])]} "
+    return carriersOnRoute
+
 min = float(sys.argv[1])
 max = float(sys.argv[2])
 print(f"Latency: {min}/{max}")
@@ -8,7 +22,7 @@ with open("targets.json", 'r') as f:
     targets =  json.load(f)
 
 count = 0
-match = []
+match = {}
 while True:
     if count > len(targets): break
     fping = ["fping", "-c", "5"]
@@ -20,7 +34,10 @@ while True:
         if "timed out" in ms: continue
         if float(ms) > min and float(ms) < max and ip not in match: 
             print(f"Found {ip}")
-            match.append(ip)
+            match[ip] = {"mtr":""}
+            print(f"Running MTR for {ip}")
+            response = mtr(ip)
+            match[ip]['mtr'] = classify(response)
     count = count + 50
     with open("match.json", 'w') as f:
-        json.dump(match, f)
+        json.dump(match, f, indent=4)
