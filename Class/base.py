2f7402aec5442f61c7a8b7341a35e7782ee20ce2
Neoon <neo@tiefkuehler-madness.me>
2023-05-28 11:48:50 +0200

    indent

--- Class/base.py
+++ Class/base.py
@@ -9,12 +9,12 @@ class Base:
 
     @staticmethod
     def cmdInitial(command,timeout=10):
-    try:
-        run = f"{command[0]} {command[1]}"
-        p = subprocess.run(run, stdin=None, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, timeout=timeout)
-        return [command[1],p.stdout.decode('utf-8'),p.stderr.decode('utf-8')]
-    except:
-        return ["",""]
+        try:
+            run = f"{command[0]} {command[1]}"
+            p = subprocess.run(run, stdin=None, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, timeout=timeout)
+            return [command[1],p.stdout.decode('utf-8'),p.stderr.decode('utf-8')]
+        except:
+            return ["",""]
 
     @staticmethod
     def SliceAndDice(notPingable,row,length=1000):
